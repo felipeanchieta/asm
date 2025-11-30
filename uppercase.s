45d6cef14b200dc2fa5dbff9200f51470da87c76
Felipe Santos <felipeanchieta2@gmail.com>
2025-11-30 17:36:16 +0100

    Create uppercase.s

---
+++ uppercase.s
@@ -0,0 +1,36 @@
+# vim: set filetype=arm64asm:
+
+#define CASE_OFFSET ('a' - 'A')
+
+  .globl _main
+  .p2align 2
+
+_main:
+  ; save frame pointer (x29) and link register (x30) to the stack
+    stp   x29, x30, [sp, #-16]!
+    mov   x29, sp 
+
+    mov   w19, wzr
+    mov   w20, 'a'
+    mov   w21, 'z'
+loop:
+    bl    _getchar
+    cmp   w0, wzr
+    b.lt  exit
+    cmp   w0, w20
+    b.hs  check_upper
+put:
+    bl    _putchar
+    b     loop
+check_upper:
+    cmp   w0, w21
+    b.ls  lowercase
+    b     put
+lowercase:
+    sub   w0, w0, #CASE_OFFSET
+    b     put
+
+exit:
+  ; restore FP and LR, move SP 16 bytes
+    ldp   x29, x30, [sp], #16
+    ret
